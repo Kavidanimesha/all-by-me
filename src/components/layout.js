e88f6d602721477f1323323feb45d53ff35a8f14
Kavidanimesha <kavindanimesha@gmail.com>
2023-02-13 20:41:49 +0530

    Folder_Structure

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,18 +3,12 @@ import { Grid } from "@mui/material";
 export default function Layout({ children }) {
   return (
     <Grid container sx={{ height: "100vh" }}>
-      <Grid
-        item
-        xs={2}
-        sx={{
-          backgroundColor: "yellow",
-        }}
-      >
+      <Grid item xs={2} sx={{ backgroundColor: "yellow" }} >
         {/*All Components in side bar goes here*/}
         <p>I am side bar</p>
       </Grid>
 
-      <Grid item xs={10}>
+      <Grid item xs={10} sx={{backgroundColor:'blue'}}>
         {children}
       </Grid>
     </Grid>
