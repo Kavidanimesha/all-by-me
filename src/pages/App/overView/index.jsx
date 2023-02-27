629bdae376369af2130362711ddad04160a84866
Kavidanimesha <kavindanimesha@gmail.com>
2023-02-28 01:25:49 +0530

    Added Charts

--- src/pages/App/overView/index.jsx
+++ src/pages/App/overView/index.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import * as echarts from 'echarts';
 
-function OverView() {
+function MyChart() {
+  useEffect(() => {
+    const chart = echarts.init(document.getElementById('my-chart'));
+
+    const options = {
+      xAxis: {
+        type: 'category',
+        data: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [{
+        data: [150, 230, 224, 218, 135, 147],
+        type: 'line',
+        // set the color of the line
+        color: 'red'
+      }]
+    };
+
+    chart.setOption(options);
+  }, []);
+
   return (
     <div>
-      OverView
+      <div id="my-chart" style={{ width: '100%', height: 300 }}></div>
     </div>
-  )
+  );
 }
 
-export default OverView
+export default MyChart;
